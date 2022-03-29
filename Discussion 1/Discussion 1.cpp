e9d64250cd73c7639603d6844819880bbd0d9080
kimsternator <sskim@ucsd.edu>
2022-03-29 12:28:46 -0700

    Clean up discussion file

--- Discussion 1/Discussion 1.cpp
+++ Discussion 1/Discussion 1.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 
-class Object2 {
+// Introducing classes (not present in C)
+class ClassObject {
 public:
-    void function1() {
+    // Functions just like inside structs
+    // This function changes the variables attached to the current instance of a class
+    void changeNum(int newNum1) {
+        num1 = newNum1;
+    }
+    void function1() const { // can add const keyword because class does not change
         std::cout << "Hello World!\n";
     }
 
@@ -12,11 +18,13 @@ public:
     std::string name;
 };
 
-int main()
-{
-    //Object2* objPtr = new Object2(); // nullptr -> 0x000234234Sdf
-    //objPtr->function1();
-    Object2 objObject{0, 1, "string"};
-    std::cout << "1\n";
-    int a = 1;
+int main() {
+    // Declaring and initializing a pointer to instance of object
+    ClassObject const* objPtr = new ClassObject(); // nullptr -> 0x000234234Sdf
+    objPtr->function1();
+    // Declaring and initializing a reference to instance of object
+    ClassObject objObject{ 0, 1, "string" };
+    // Must delete our pointer
+    // Falling out of scope deletes the variable but our variable is a pointer so we need to delete the object instance
+    delete objPtr;
 }
