4b89f1e4aa62edcef0fa66b4c3e99db368d2571e
kimsternator <sskim@ucsd.edu>
2022-04-11 10:10:30 -0700

    Initial Discussion 3

---
+++ Discussion 3/Discussion 3.cpp
@@ -0,0 +1,68 @@
+// Discussion 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+streams
+strings
+
+encapsulation
+inheritance
+polymorphism
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void strings() {
+	// (), =
+	
+	// length, size
+
+	// []
+
+	// pa2 -> replace, +=, insert, find
+}
+
+void streams() {
+	// input, output
+
+	// file input, output
+
+	// string
+}
+
+class Encapulation {
+public:
+
+private:
+};
+
+class Animal {
+public:
+	void speak() {
+		std::cout << "???\n";
+	}
+private:
+};
+
+class Dog : public Animal {
+public:
+
+private:
+};
+
+void inheritance() {
+
+}
+
+void polymorphism() {
+
+}
+
+int main() {
+	strings();
+	streams();
+	inheritance();
+	polymorphism();
+}
